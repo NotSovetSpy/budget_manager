7029ce273d8ca6b1dd1c3b3c1797115271cb1e76
Matej Petuchovsky <petuchovsky@gmail.com>
2024-07-14 20:07:35 +0200

    interface prototype v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use budget_manager::run;
+use crossterm::event::{EnableMouseCapture, Event};
+use crossterm::execute;
+use crossterm::terminal::{enable_raw_mode, Clear, ClearType, EnterAlternateScreen, SetTitle};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup terminal
+    enable_raw_mode()?;
+    let mut stdout = std::io::stdout();
+    execute!(
+        stdout,
+        EnterAlternateScreen,
+        EnableMouseCapture,
+        Clear(ClearType::All),
+        SetTitle("Budget Manager")
+    )?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    run(&mut terminal)
 }
